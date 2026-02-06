d9977bbb3547276fbd2f912605d23d6fe53232ca
xansec <abrewer@forallsecure.com>
2026-02-06 14:54:20 -0500

    working dind example

--- mdsbom/run_mdsbom.sh
+++ mdsbom/run_mdsbom.sh
@@ -8,12 +8,14 @@ echo "api_token = \"$MAYHEM_TOKEN\"" >> /etc/mdsbom/config.toml
 echo "workspace = \"$WORKSPACE\"" >> /etc/mdsbom/config.toml
 
 mdsbom login $MAYHEM_URL $MAYHEM_TOKEN
+mdsbom server > /var/log/mdsbom-server.log &
+sleep 5
 docker pull $API_IMAGE
 docker run --runtime=mdsbom $API_IMAGE &
 PID=$!
 sleep 5
 kill -INT $PID
-if ! mdsbom query --format csv --local images | grep -q $API_IMAGE; then
+if ! mdsbom query --format csv --local containers -a | grep -q $API_IMAGE; then
   echo "ERROR: Image $API_IMAGE has not been recorded."
   exit 1
 fi
